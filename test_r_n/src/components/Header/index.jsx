0c61ddc969d483947a2c70892c3b2e9c8d5de746
Diana Murtazina <murka8831@yandex.ru>
2024-03-29 23:21:34 +0300

    fix goBack button

--- test_r_n/src/components/Header/index.jsx
+++ test_r_n/src/components/Header/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../common/Button/Button";
 import classes from "./Header.module.css";
 
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const goBack = () => {
+        if (!location?.pathname.includes("/post")) return;
+        navigate(-1);
+    };
     return (
-        <div className={classes.header} onClick={() => navigate(-1)}>
+        <div className={classes.header} onClick={goBack}>
             <Button>назад</Button>
         </div>
     );
